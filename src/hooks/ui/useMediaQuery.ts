f517ca82dcfd5253d77ebbed49146dbac944b347
kristley <kriss.1999@hotmail.com>
2024-11-28 23:00:41 +0100

    omfg prettierr

--- src/hooks/ui/useMediaQuery.ts
+++ src/hooks/ui/useMediaQuery.ts
@@ -1,19 +1,19 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const useMediaQuery = (query: string) => {
   const [matches, setMatches] = useState(false);
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
       const mediaQuery = window.matchMedia(query);
       setMatches(mediaQuery.matches);
 
       const handler = (event: MediaQueryListEvent) => setMatches(event.matches);
-      mediaQuery.addEventListener('change', handler);
+      mediaQuery.addEventListener("change", handler);
 
-      return () => mediaQuery.removeEventListener('change', handler);
+      return () => mediaQuery.removeEventListener("change", handler);
     }
   }, [query]);
 
