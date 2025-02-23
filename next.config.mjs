b9cf29ee3e91f8121f2070961eeefe241515d979
donadelicc <preb1anders1@gmail.com>
2025-02-23 19:34:52 +0100

    hey2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+
 const nextConfig = {
     images: {
         remotePatterns: [
@@ -6,7 +10,7 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'cdn.sanity.io',
                 port: '',
-                pathname: '/images/gjirgpbe/production/**'
+                pathname: `/images/${projectId}/${dataset}/**`
             }
         ]
     }
